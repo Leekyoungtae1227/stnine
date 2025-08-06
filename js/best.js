acfba7cd45b084914c1fd03b70e13da0c201aac1
이경태 <lkt1227@naver.com>
2025-08-06 17:41:54 +0900

    저장1

---
+++ js/best.js
@@ -0,0 +1,53 @@
+fetch("js/best.json")
+.then((res) => res.json())
+.then(rs => {
+
+  let slidePage = "";
+  let colPage = "";
+  for(let i=0; i<3; i++){
+    colPage = "";
+
+    for(let j=0; j<8; j++){
+      const index = i * 8 + j;
+      const data = rs[index];
+      //색상처리
+      let colorHtml ="";
+      data.color.forEach(co => {
+        colorHtml += `<span class="${co}"></span>`;
+      });
+      //상품목록 만들기
+      colPage += `
+                  <div class="col-md-3 my-3">
+                  <a href="#" class="img-best-box">
+                    <img src="${data.img}" alt="${data.alt}">
+                    <div class="pd-best-box text-center">
+                    <div class="pd-color">
+                    ${colorHtml}</div>
+                      <div class="best-title">${data.title}</div>
+                      <div class="pd-best-pay">
+                        <del>${data.cost}원</del>
+                        <span class="sail">${data.sale}</span>
+                        <span class="money">${data.price}원</span>
+                      </div>
+                    </div>
+                    <div class="btn-box">
+                      <button type="button" class="best-cart">
+                        <i class="ri-shopping-bag-4-line"></i>
+                      </button>
+                      <button type="button" class="best-heart">
+                        <i class="ri-heart-line"></i>
+                      </button>
+                    </div>
+                  </a>
+                </div>`;
+
+    }
+
+    slidePage += `<div class="row slide-page">${colPage}</div>`;
+  }
+
+  document.querySelector(".slide-wrapper-in").innerHTML = slidePage;
+
+})
+.catch((err) => { console.error("JSON 로딩 에러: ", err);
+});
--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
 $(function(){
+let wrapperWidth = 0;
+let pgConst =0;
+const totalPage =3;
+
+
+
+//페이지 버튼 생성
+
+for(let i=0; i <totalPage; i++){
+    if(i==0){
+        $('#page').append(`<li date-index="${i}" class="active"></li>`);
+    }else{
+        $('#page').append(`<li date-index="${i}"></li>`);
+    }
+}
+
+
+
+
    $('.nav-item').hover(function(){
      $(this).find('ul.lnb').fadeToggle();
    });
@@ -8,6 +27,80 @@ $(function(){
       $('.categorybox').css('width', w+"px"); //categorybox의 가로 크기로 설정함.
       $(this).find('.categorybox').fadeToggle();
    });
+
+$(document).on('mouseenter', '.best-cart', function(){
+    $(this).find('i').removeClass('ri-shopping-bag-4-line').addClass('ri-shopping-bag-4-fill');
+}).on('mouseleave', '.best-cart', function(){
+    $(this).find('i').removeClass('ri-shopping-bag-4-fill').addClass('ri-shopping-bag-4-line');
+});
+
+$(document).on('mouseenter', '.best-heart', function(){
+    $(this).find('i').removeClass('ri-heart-line').addClass('ri-heart-fill');
+}).on('mouseleave', '.best-heart', function(){
+    $(this).find('i').removeClass('ri-heart-fill').addClass('ri-heart-line');
+});
+
+   $(window).on('scroll', function(){
+      const navigation = $('.navigation').offset().top;
+      if($(this).scrollTop() > 220){
+         $('.navigation').css({
+            position : 'fixed',
+            top: '45px',
+            width: '100%'
+         });
+      }else{
+         $('.navigation').css({
+            position: 'static'
+         })
+      }
+   });
+
+
+$(window).on('load', function(){
+
+    const pageHeight = $('.slide-page:first-child').outerHeight(true);
+    $('.slide-wrapper').css('height', pageHeight+"px");
+    wrapperWidth = $('.slide-wrapper').width();
+});
+
+setInterval(bestSilde, 8000);
+
+
+function updatePage(){
+    $('#page li').removeClass('active')
+        .eq(pgCount).addClass('active')
+    
+}
+
+
+
+
+
+
+function bestSilde(){
+    pgConst++;
+    if(pgConst == totalPage){
+        pgConst =0;
+    }
+
+
+
+
+    $('.slide-wrapper-in').animate({
+        left : -wrapperWidth + "px"
+    }, 300, function(){
+
+        const first = $('.slide-wrapper-in .slide-page').first();
+        first.clone().appendTo('.slide-wrapper-in');
+        first.remove();   
+        $('.slide-wrapper-in').css('left', 0);
+    });
+}
+
+
+
+
+
 
 });
 
